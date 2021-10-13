ef4584130b0d06d4d970fa8f84088b27ed4b0dc6
Trenton Isenor <ronesinotnert@gmail.com>
2021-10-13 08:58:08 -0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FIZZBUZZ
+{
+    class Program
+    {
+        static string output = "";
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 100; i++)
+            {
+                output = "";
+                if ((i % 3) == 0) { output += "FIZZ"; }
+                if ((i % 5) == 0) { output += "BUZZ"; }
+                if ((i % 7) == 0) { output += "FUZZ"; }
+                else if ((i % 3) != 0 && (i % 5) != 0 && (i % 7)!= 0) { output = i.ToString(); }
+                Console.WriteLine(output);
+                Console.ReadKey(true);
+            }
+        }
+    }
+}
